09a16d51ea947b92931cb64d1e706ca8bbc90ad4
SaulBgitgrub <saulbrito342@gmail.com>
2026-01-27 15:35:19 +0000

    failure

--- 4_dynamicFunctions.py
+++ 4_dynamicFunctions.py
@@ -15,7 +15,11 @@ def all_positives(list):
 
 # Dynamic Functions Practice #2
 # Create a function (sum_less) that adds the numbers of a list as long as they are greater than 0 and less than 1000, and returns the result of said sum. Create a numbers variable, storing a list of numbers so we can test it.
-
+list2 = [0, 5, 7, 2000]
+def sum_less(list2):
+    for sum in list2:
+        if sum < 1000 and sum > 0:
+            
 
 
 
