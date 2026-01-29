344c7187e3347ede3446171b22986a0478a6d99f
SaulBgitgrub <saulbrito342@gmail.com>
2026-01-29 14:43:09 +0000

    Learning args kwargs

--- 7_indefinite_arguments_args.py
+++ 7_indefinite_arguments_args.py
@@ -1,5 +1,39 @@
 # Indefinite Arguments (*args) Practice #1
 # Create a function called sum_squares that takes any number of numeric arguments, and returns the sum of their values squared.
+# def sum_squares(4,5,6):
+#     for number in sum_squares:
+
+# def tea_order(customer_name, tea_type, *args):
+#     print(customer_name, "ordered a", tea_type, "tea")
+#     print("Args contains: ", args)
+#     for arg in args:
+#         print("   - add:", arg)
+
+# tea_order("Alice","chamomile")
+# tea_order("Bob", "black","oat")
+# tea_order("Tony","Black", "Oat", "Honey")
+
+# def tea_order(customer_name, tea_type, **kwargs):
+#     print(customer_name, "ordered a", tea_type, "tea")
+#     for key, value in kwargs.items():
+#         print("   - add:", key, ":", value)
+
+# tea_order("Alice","chamomile")
+# tea_order("Bob", "black",milk="oat")
+# tea_order("Tony","Black", milk="Oat", sweetener="Honey")
+
+def tea_order(customer_name, tea_type, *args, **kwargs):
+    print(customer_name, "ordered a", tea_type, "tea")
+    for arg in args:
+        print("   - Add", arg)
+    for key, value in kwargs.items():
+        print("   - add:", key, ":", value)
+
+eves_extras = ("milk": "almond", "sweetener": "sugar", "flavor": "lemon")
+tea_order("Eve", "green", **eves_extras)
+tea_order("Alice","chamomile")
+tea_order("Bob", "black",milk="oat")
+tea_order("Tony","Black", milk="Oat", sweetener="Honey")
 
 # For example for the arguments sum_squares(1,2,3) it should return 14 (1+4+9).
 
